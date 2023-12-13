1c3dbb4d0387b0ceeea6807de758a3000a2d82d0
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2023-12-13 17:55:47 +0600

    firebase deploy done

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://assignment12-server-coral.vercel.app",
 });
 
 const useAxiosPublic = () => {
